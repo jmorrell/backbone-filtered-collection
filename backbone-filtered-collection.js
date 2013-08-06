d5df76e1647ca7c80c05a2b0b182beae897a94e7
Jeremy Morrell <morrell.jeremy@gmail.com>
2013-08-06 00:21:01 -0500

    More implementation. Most tests passing.
    Hardest ones yet to come + need to define event behavior.

--- backbone-filtered-collection.js
+++ backbone-filtered-collection.js
@@ -1,90 +1,27 @@
 /*! backbone-filtered-collection v0.0.0 - MIT license */
 ;(function (root) { function moduleDefinition(Backbone, _) {
 
-  /*
-
-function initialize(models, options) {
-  this._superset = options.superset;
-  this.on('change', filter, this);
-
-  this.filters = {};
-
-  bindToParentCollection.call(this);
-  filter.call(this);
-}
-
-function bindToParentCollection() {
-  this._superset.on('change reset add', this._filter, this);
-}
-
-function filterFunction(model) {
-  var bools = _.map(this.filters, function(filter, key) {
-    return filter(model);
-  });
-
-  // The model passes if all of the tests were `true`, false otherwise.
-  // This will return true when bools is an empty array.
-  return _.all(bools, _.identity);
-}
-
-function filter() {
-  var filtered = [];
-  
-  // Filter the collection
-  if (this._superset) {
-    filtered = this._superset.filter(_.bind(filterFunction, this));
+// TODO: Describe the three otions for filter, and how it works
+function convertFilterToFunction(filter) {
+  if (_.isFunction(filter)) {
+    return filter;
+  } else if (_.isObject(filter)) {
+    return function(model) {
+      for (var key in filter) {
+        if (_.isFunction(filter[key])) {
+          if (!filter[key](model.get(key))) {
+            return false;
+          }
+        } else {
+          if (filter[key] !== model.get(key)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
   }
-
-  this.reset(filtered);
 }
-
-function Filtered(superset) {
-  this._superset = superset;
-  this.filters = {};
-
-  this.on('change', filter, this);
-  this._superset.on('change reset add', filter, this);
-}
-
-_.extend(Filtered, {
-
-  filterBy: function(filterName, filter) {
-    if (_.isFunction(filter)) {
-      this.filters[filterName] = filter;
-    } else if (filter.key && filter.expected && _.isFunction(filter.expected)) {
-      this.filters[filterName] = function(model, collection, i) {
-        return filter.expected(model.get(filter.key), filter.key);
-      };
-    } else if (filter.key && filter.expected) {
-      this.filters[filterName] = function(model, collection, i) {
-        return model.get(filter.key) === filter.expected;
-      };
-    }
-    this.trigger('change');
-
-    return this;
-  },
-
-  removeFilter: function(filterName) {
-    delete this.filters[filterName];
-    this.trigger('change');
-
-    return this;
-  },
-
-  resetFilters: function() {
-    this.filters = {};
-    this.trigger('change');
-
-    return this;
-  },
-
-  superset: function() {
-    return this._superset;
-  }
-
-});
-*/
 
 function filterFunction(model) {
   var bools = _.map(this._filters, function(filter, key) {
@@ -121,43 +58,39 @@ function Filtered(superset) {
   this._superset.on('change reset add', filter, this);
 }
 
+var parameters = {
+  defaultFilterName: '__default'
+};
+
 var methods = {
 
   filterBy: function(filterName, filter) {
+    // Allow the user to skip the filter name if they're only using one filter
     if (!filter) {
       filter = filterName;
-      filterName = "__default";
+      filterName = this.defaultFilterName;
     }
 
-    if (_.isFunction(filter)) {
-      this._filters[filterName] = filter;
-    } else if (_.isObject(filter)) {
-      this._filters[filterName] = function(model) {
-        for (var key in filter) {
-          if (filter[key] !== model.get(key)) {
-            return false;
-          }
-        }
-        return true;
-      };
-    }
+    this._filters[filterName] = convertFilterToFunction(filter);
 
     this.trigger('change');
-
     return this;
   },
 
   removeFilter: function(filterName) {
+    if (!filterName) {
+      filterName = this.defaultFilterName;
+    }
     delete this._filters[filterName];
-    this.trigger('change');
 
+    this.trigger('change');
     return this;
   },
 
   resetFilters: function() {
     this._filters = {};
-    this.trigger('change');
 
+    this.trigger('change');
     return this;
   },
 
@@ -170,7 +103,7 @@ var methods = {
 // Methods on `this._collection` we will expose to the outside world
 var collectionMethods = [
   'toJSON', 'first', 'last', 'at', 'get', 'map',
-  'each', 'slice', 'where', 'findWhere'
+  'each', 'slice', 'where', 'findWhere', 'contains'
 ];
 
 _.each(collectionMethods, function(method) {
@@ -179,15 +112,10 @@ _.each(collectionMethods, function(method) {
   };
 });
 
-
-
-
-
-
 _.extend(Filtered.prototype, methods, Backbone.Events);
+_.extend(Filtered, parameters);
 Filtered.extend = Backbone.Collection.extend;
 return Filtered;
-
 
 // ---------------------------------------------------------------------------
 } if (typeof exports === 'object') {
