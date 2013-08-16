227a1fffa7c9f4bc826a1d453c8c3891c7aa64d5
Jeremy Morrell <morrell.jeremy@gmail.com>
2013-08-15 21:15:39 -0500

    More development on the map example.

--- examples/map/js/script.js
+++ examples/map/js/script.js
@@ -1,3 +1,8 @@
+var tileUrl = 'http://{s}.tile.cloudmade.com/' +
+              'c4157c3f815445f4a5d9e3c55eafadc0/997/256/{z}/{x}/{y}.png';
+
+var startLatitude = 37.7750;
+var startLongitude = -122.4167;
 
 var TableView = Backbone.View.extend({
   tagName: 'table',
@@ -12,23 +17,52 @@ var TableView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html(this.template());
+    this.el.innerHTML = this.template();
+    var tbodyEl = this.el.getElementsByTagName('tbody')[0];
 
     var tbody = [];
-    this.collection.each(function(model) {
+    var models = this.collection.slice(0, 20);
+    _.each(models, function(model) {
       tbody.push(this.rowTemplate(model.toJSON()));
     }, this);
 
     if (tbody.length) {
-      this.$('tbody').html(tbody.join(''));
+      tbodyEl.innerHTML = tbody.join('');
     } else {
-      this.$('tbody').html(this.emptyTemplate());
+      tbodyEl.innerHTML = this.emptyTemplate();
     }
   }
 });
 
-var MapView = Marionette.ItemView.extend({
+var MapView = Backbone.View.extend({
 
+  initialize: function() {
+    // initialize map view
+    this.map = L.map('map').setView([startLatitude, startLongitude], 5);
+    L.tileLayer(tileUrl, {
+      attribution: '',
+      maxZoom: 18
+    }).addTo(this.map);
+
+    // add listener for map drag
+    this.map.on('drag', _.bind(_.throttle(this.onDrag, 100), this));
+    this.map.on('dragend', _.bind(this.onDrag, this));
+    this.map.on('zoomend', _.bind(this.onDrag, this));
+    this.onDrag();
+  },
+
+  onDrag: function(ev) {
+    // get the new map boundaries and add / modify the filter
+    var bounds = this.map.getBounds();
+    this.collection.filterBy('lat long', function(model) {
+      return (
+        +model.get('latitude') > bounds.getSouth() &&
+        +model.get('latitude') < bounds.getNorth() &&
+        +model.get('longitude') < bounds.getEast() &&
+        +model.get('longitude') > bounds.getWest()
+      );
+    });
+  }
 });
 
 var dataCollection = new Backbone.Collection(data);
@@ -41,7 +75,8 @@ var tableView = new TableView({
 
 var mapView = new MapView({
   collection: filtered,
-  el: '.map'
+  el: '#map'
 });
 
 tableView.render();
+mapView.render();
